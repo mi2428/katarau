5320cde67c89c077e450d3f25956f8ce16a0ab1a
derpda <peter.spalthoff@gmail.com>
2018-12-14 16:20:18 +0900

    Landing page initial setup

--- src/views/Landing/index.vue
+++ src/views/Landing/index.vue
@@ -1,20 +1,40 @@
 <template>
-  <v-content>
-    <v-container>
-      <v-layout row>
-        <v-flex xs12>
-          <h1>KATARAU</h1>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-content>
+  <div class="landing">
+    <v-layout row justify-space-around align-center>
+      <v-flex xs2>
+        <v-btn ripple
+          large
+          @click="to_students()">学生
+        </v-btn>
+      </v-flex>
+      <v-flex xs2>
+        <v-btn ripple
+          large
+          @click="to_elderly()">高齢者
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Landing",
+  methods: {
+    to_elderly() {
+      this.$router.push({path : 'elderly'})
+    },
+    to_students() {
+      this.$router.push({path : 'students'})
+    },
+  }
 };
 </script>
 
 <style scoped lang="scss">
+.landing {
+  height: 100vh;
+  background-image: url('~@/assets/landing_background.jpg');
+  background-size: cover;
+}
 </style>
