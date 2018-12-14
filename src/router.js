5feb47b266cbf924994afdf3020ed68be1f9137d
Ayako Tamaki <ayako@MacBook-Air-san.local>
2018-12-14 21:39:05 +0900

    making elderly padges

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Landing from "./views/Landing/index.vue"
 import Home from "./views/Home/index.vue"
+import Student from "./views/Student/index.vue"
+import Elderly from "./views/Elderly/index.vue"
 import Layout from "@/views/Layout";
 
 
@@ -12,16 +14,16 @@ const router = new Router({
   base: process.env.BASE_URL,
     routes: [
       {
+        path: '/landing',
+        name: 'landing',
+        component: Landing
+      },
+      {
         path: '/',
         name: 'layout',
         component: Layout,
         redirect: { name: "landing"},
         children: [
-          {
-            path: '/landing',
-            name: 'landing',
-            component: Landing
-          },
           {
             path: '/home',
             name: 'home',
@@ -34,6 +36,16 @@ const router = new Router({
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
             component: () => import(/* webpackChunkName: "about" */ './views/About/index.vue')
+          },
+          {
+            path: "/students",
+            name: "students",
+            component: Student
+          },
+          {
+            path: "/elderly",
+            name: "elderly",
+            component: Elderly
           }
         ]
       }
