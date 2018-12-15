9835670b9ea4a684c90cf5ab36410b3366de4172
unagi-is-god <keiya.v1046-r@mbn.nifty.com>
2018-12-15 11:58:22 +0900

    modified regist form

--- src/views/join/components/registration_form.vue
+++ src/views/join/components/registration_form.vue
@@ -2,41 +2,76 @@
   <div>
     <section>
       <v-card>
-          <h2>name</h2>
-        <header>
-            <v-text-field v-model="lastname"
-            placeholder="name"
+        <v-layout row>
+          <v-flex xs3>
+            <v-subheader>名前</v-subheader>
+          </v-flex>
+        　<v-flex xs12 sm3>
+            <v-text-field
+              v-model="lastname"
+              placeholder="性"
             ></v-text-field>
-        </header>
-        <h2>電話番号</h2>
-        <header>
-            <v-text-field v-model="firstname"
-            placeholder="電話番号"
+          </v-flex>
+          <v-flex xs12 sm3>
+            <v-text-field
+              v-model="lastname"
+              placeholder="名"
             ></v-text-field>
-        </header>
-        <h2>メールアドレス</h2>
-        <header>
-            <v-text-field v-model="email"
-            placeholder="メールアドレス"
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs3>
+            <v-subheader>電話番号</v-subheader>
+          </v-flex>
+        　<v-flex xs12 sm6>
+            <v-text-field
+              v-model="phone"
+              placeholder="電話番号"
             ></v-text-field>
-        </header>
-        <h2>専攻</h2>
-        <header>
-            <v-text-field v-model="special"
-            placeholder="選考"
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs3>
+            <v-subheader>メールアドレス</v-subheader>
+          </v-flex>
+        　<v-flex xs12 sm6>
+            <v-text-field
+              v-model="email"
+              placeholder="e-mailアドレス"
             ></v-text-field>
-        </header>
-        <h2>パスワード</h2>
-        <header>
-            <v-text-field v-model="password"
-            placeholder="パスワード"
+          </v-flex>
+        </v-layout>
+
+        <v-layout row>
+          <v-flex xs3>
+            <v-subheader>専攻</v-subheader>
+          </v-flex>
+        　<v-flex xs12 sm6>
+            <v-text-field
+              v-model="special"
+              placeholder="専攻"
             ></v-text-field>
-        </header>
-            <v-card-actions>
-               <v-btn grey left @click="registration()">登録する</v-btn>
-            </v-card-actions>
-            <v-icon>person</v-icon>
-            <v-card>{{firstname}}</v-card>
+          </v-flex>
+        </v-layout>
+        
+        <v-layout row>
+          <v-flex xs3>
+            <v-subheader>パスワード</v-subheader>
+          </v-flex>
+        　<v-flex xs12 sm6>
+            <v-text-field
+              v-model="password"
+              placeholder="パスワード"
+            ></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout justify-center>
+          <v-card-actions left>
+            <v-btn red @click="registration()">登録する</v-btn>
+          </v-card-actions>
+        </v-layout>
       </v-card>
     </section>
   </div>
@@ -66,6 +101,8 @@
             firebase.firestore().collection("user").add({
               firstname: this.firstname,
               lastname: this.lastname,
+              phone: this.phone,
+              special: this.special
             });
 
           })
